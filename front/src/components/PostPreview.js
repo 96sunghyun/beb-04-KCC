6da54ed8354491819b2825e9603ac46d34b0bdcd
Jinwoo Cheon <jinwoocheon@yonsei.ac.kr>
2022-06-21 19:26:22 +0900

    Add post id routing

--- front/src/components/PostPreview.js
+++ front/src/components/PostPreview.js
@@ -1,8 +1,10 @@
-export default function PostPreview({ title, creator }) {
+import { Link } from "react-router-dom";
+
+export default function PostPreview({ title, creator, postId }) {
     return (
-        <div className="flex justify-between w-full p-2">
+        <Link to={`/post/${postId}`} className="flex justify-between w-full p-2 border-y border-dashed hover:bg-[#F2EDD7] hover:text-[#755139] hover:cursor-pointer">
             <div>{title}</div>
             <div>{creator}</div>
-        </div>
+        </Link>
     )
 }
