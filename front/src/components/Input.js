b911c827ae80a69e03307efa43400ab7625d276a
Jinwoo Cheon <jinwoocheon@yonsei.ac.kr>
2022-06-23 00:16:26 +0900

    Add login & mypage

--- front/src/components/Input.js
+++ front/src/components/Input.js
@@ -1,6 +1,6 @@
 export default function Input({ inputName = "null", onChange, value }) {
     return (
-        <div className="flex space-x-4">
+        <div className="flex flex-col">
             <div>{inputName}</div>
             <input onChange={(e) => onChange(e.target.value)} value={value} type="text" className="text-[#755139]" />
         </div>
--- front/src/pages/Login.js
+++ front/src/pages/Login.js
@@ -1,3 +1,55 @@
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+import WrapperBasic from "../components/WrapperBasic";
+import WrapperBody from "../components/WrapperBody";
+import Input from "../components/Input";
+import Button from "../components/Button";
+import Message from "../components/Message";
+
+import { useEffect, useState } from "react";
+
+const DEFAULT_BUTTON_TEXT = "Input ID/PW";
+
 export default function Login() {
-    return "Login";
+
+    // Signup POST failed?
+    const [fail, setFail] = useState(false);
+
+    const [buttonText, setButtonText] = useState(DEFAULT_BUTTON_TEXT)
+    const [disabled, setDisabled] = useState(true);
+
+    const [id, setId] = useState("");
+    const [pw, setPw] = useState("");
+
+    useEffect(() => {
+        if (id && pw) {
+            setDisabled(false);
+            setButtonText("Login!");
+        } else {
+            setDisabled(true);
+            setButtonText(DEFAULT_BUTTON_TEXT);
+        }
+    }, [id, pw])
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        console.log(id, pw);
+    }
+
+    return (
+        <WrapperBasic>
+            <Navbar backButton={true} />
+            <WrapperBody>
+                <div className="flex flex-col items-center space-y-6 w-full my-4">
+                    <div className="flex flex-col space-y-2 justify-center items-center">
+                        <Input inputName="ID" value={id} onChange={setId} />
+                        <Input inputName="PW" value={pw} onChange={setPw} />
+                    </div>
+                    <Button name={buttonText} isSubmit={true} onSubmit={onSubmit} disabled={disabled} />
+                    { fail ? <Message m="Login failed." /> : null }
+                </div>
+            </WrapperBody>
+            <Footer />
+        </WrapperBasic>
+    )
 }
